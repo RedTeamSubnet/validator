9bcf01726e7f3ea02638a773a7f4b87a99e57c9b
aliyuldashev <javohir.hbai@gmail.com>
2026-01-10 10:52:55 +0900

    chore: load all configuration for standalone mode

--- src/validator/__init__.py
+++ src/validator/__init__.py
@@ -1,7 +1,9 @@
 from .__version__ import __version__
 from ._core import Validator
 from ._base import BaseValidator
+from dotenv import load_dotenv
 
+load_dotenv()
 __all__ = [
     "__version__",
     "Validator",
